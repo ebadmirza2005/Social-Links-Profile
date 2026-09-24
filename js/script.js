3d3593c08bba6d7121a7ad8bbec67f8b8e9bfd06
Ebad Mirza <ebadmirza.2005@gmail.com>
2026-09-24 10:25:08 +0500

    Fixed Logic

--- js/script.js
+++ js/script.js
@@ -1,29 +1,14 @@
 const socialLinks = document.querySelectorAll('.social-links');
-const githubLink = document.querySelector('.github');
-const frontendMentorLink = document.querySelector('.frontend-mentor');
-const instagramLink = document.querySelector('.instagram');
+
 
 socialLinks.forEach(link => {
     link.addEventListener('click', (e) => {
         e.preventDefault();
-        e.target.classList.add('active');
         socialLinks.forEach(l => l.classList.remove('active'));
         link.classList.add('active');
+
+        const url = link.dataset.url;
+        if (url) window.open(url, '_blank');
     })
-})
-
-githubLink.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.open('https://github.com/ebadmirza2005', '_blank');
-})
-
-frontendMentorLink.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.open('https://www.frontendmentor.io/profile/ebadmirza2005', '_blank');
-})
-
-instagramLink.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.open('https://www.instagram.com/ebadmirza_2005/', '_blank');
 })
 
